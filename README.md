2e71bf835fabc6c26684a42bfda8d3f9f7b854e5
perchik_air <1919ars@gmail.com>
2021-09-19 22:09:32 +0300

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask with DB deployed to Heroku
+
+# Credits
+
+- [YouTube | Build & Deploy A Python Web App | Flask, Postgres & Heroku](https://www.youtube.com/watch?v=w25ea_I89iM&t=302s)
---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+# ENV = 'dev'
+ENV = 'prod'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/heroku_flask_db'
+if ENV == 'prod':
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://skwmlusblqxfdx:92ab2cde87bc3198fd856ce7a543503d7be0c7411ac57c9b3029a6ccff9a5b0f@ec2-3-214-3-162.compute-1.amazonaws.com:5432/d9q38bu4qri2s1'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, comments):
+        self.customer = customer
+        self.comments = comments
+
+
+@app.route('/success')
+def success_func():
+    return render_template('success.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit_func():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        comments = request.form['comments']
+        form_data = [customer, comments]
+        if customer == '' or comments == '':
+            return render_template('index.html', message='Please enter required fields...')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer=customer, comments=comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html', form_data=form_data)
+        return render_template('index.html', message='You have already submitted feedback...')
+    return render_template('index.html')
+
+
+@app.route('/')
+def index_func():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
